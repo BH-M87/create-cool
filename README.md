6aa391fccd32d2a8ff87c8fddae9bce65727fe5a
UrsaMino <liuxuheng1990@gmail.com>
2019-04-21 09:49:16 +0000

    project init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # create-cool
+
+Part of the [cool-cli](https://github.com/BH-M87/cool-cli).
+
+Alias for `cool-cli init`, so you could use `npm init cool`.
---
+++ bin/create-cool.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const runScript = require('../utils/runScript');
+
+runScript('cool-cli/bin/init', ['--skipupdate']);
