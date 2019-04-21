9a18651b0bb16410d629593e4a5b37af093ac791
Xuheng Liu <liuxuheng1990@gmail.com>
2019-04-21 21:07:11 +0800

    feat: pass argv to cool-cli

--- bin/create-cool.js
+++ bin/create-cool.js
@@ -3,5 +3,11 @@
 'use strict';
 
 const runScript = require('../utils/runScript');
+const argv = require('yargs').argv;
 
-runScript('cool-cli/scripts/init', ['--skipupdate']);
+runScript(
+  'cool-cli/scripts/init',
+  ['--skipupdate', '--create-cool'].concat(
+    Object.keys(argv || {}).map(key => `--${key}=${argv[key]}`)
+  )
+);
